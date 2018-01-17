30736f79e68a8a8eefb96853694f25fe1924e60b
Ehron Gwinn <ehron@local>
2018-01-17 15:27:35 -0500

    add firewall rules

--- main.tf
+++ main.tf
@@ -2,16 +2,47 @@ provider "google" {
   region = "${var.region}"
 }
 
+# Create VPC
 module "demo-vpc" {
   source                  = "tasdikrahman/network/google"
   name                    = "${var.network_name}"
   auto_create_subnetworks = false
 }
 
+# Create Subnets
 module "subnet-demo-vpc" {
   source            = "tasdikrahman/network-subnet/google"
   name              = "${module.demo-vpc.name}-${var.region}"
   vpc               = "${module.demo-vpc.self_link}"
   subnetwork-region = "${var.region}"
   ip_cidr_range     = "${var.ip_cidr_range}"
+}
+
+# Create firewall rules
+resource "google_compute_firewall" "allow-http" {
+  name    = "test-firewall-http"
+  network = "${module.demo-vpc.name}"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+
+  source_tags = ["web"]
+}
+
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "test-firewall-ssh"
+  network = "${module.demo-vpc.name}"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_tags = ["web"]
 }
