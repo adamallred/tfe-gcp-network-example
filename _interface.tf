15ef5392388faba0955ac75d25ab7c624280b9bc
Ehron Gwinn <ehron@local>
2018-01-17 13:40:55 -0500

    Don't auto-create subnets

--- _interface.tf
+++ _interface.tf
@@ -7,10 +7,14 @@ variable "network_name" {
   description = "The name of the network to be created."
 }
 
+variable "ip_cidr_range" {
+  description = "The IP range to create."
+}
+
 output "network_self_link" {
-  value = "${module.this.self_link}"
+  value = "${module.demo-vpc.self_link}"
 }
 
 output "name" {
-  value = "${module.this.name}"
+  value = "${module.demo-vpc.name}"
 }
--- main.tf
+++ main.tf
@@ -2,8 +2,16 @@ provider "google" {
   region = "${var.region}"
 }
 
-module "this" {
+module "demo-vpc" {
   source = "tasdikrahman/network/google"
   name   = "${var.network_name}"
-  auto_create_subnetworks = true
+  auto_create_subnetworks = false
+}
+
+module "subnet-demo-vpc" {
+  source            = "tasdikrahman/network-subnet/google"
+  name              = "${module.demo-vpc.name}-${var.region}"
+  vpc               = "${module.demo-vpc.self_link}"
+  subnetwork-region = "${var.region}"
+  ip_cidr_range     = "${var.ip_cidr_range}"
 }
